90700985d95d9918bc61508963724ce4a2083bb2
Renan Pupin <renan.pupin@gmail.com>
2023-01-05 20:55:41 -0300

    examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
